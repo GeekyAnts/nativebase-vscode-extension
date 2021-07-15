3d93cfcd318a5d7380d44767aa5b52b9ab2be463
cyrus25 <cyruspassi@gmail.com>
2021-07-15 17:37:08 +0530

    Remove out folder

--- out/theme/utils/resolveColors.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const resolveColors = () => {
-    return 'coll';
-};
-exports.default = resolveColors;
-//# sourceMappingURL=resolveColors.js.map
